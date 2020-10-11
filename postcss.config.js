95b97dc7857eeda188d9708227973355e9d3187d
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-10-11 09:33:40 +0200

    feat: purge 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,7 @@
 const purgecss = [
-  'postcss-flexbugs-fixes',
-  [
-    'postcss-preset-env',
-    {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false,
-      },
-    },
-  ],
   '@fullhuman/postcss-purgecss',
   {
-    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    content: ['./components/**/*.js', './pages/**/*.js'],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
