ae8b9622bdd01fdb92ab92b9914a77e2d3ab0804
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-10-11 09:09:13 +0200

    feat: purge 1

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,21 @@
 const purgecss = [
+  'postcss-flexbugs-fixes',
+  [
+    'postcss-preset-env',
+    {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  ],
   '@fullhuman/postcss-purgecss',
   {
-    content: ['./components/**/*.js', './pages/**/*.js'],
-    defaultExtractor:
-      // (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      (content) => {
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-        // Capture classes within other delimiters like .block(className="w-1/2") in Pug
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-        return broadMatches.concat(innerMatches);
-      },
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
 module.exports = {
