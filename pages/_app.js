c9288d3b93f63ad7e7a11355fed6ccd2c2c7c2f8
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-10-11 13:02:35 +0200

    feat: purge 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 /* eslint react/prop-types: 0, react/react-in-jsx-scope: 0 */
-import '../styles/index.css';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: {
-    layers: ['utilities'],
-    content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  },
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333',
-      },
+    mode: 'all',
+    content: ['./src/**/*.{js,mdx}', './next.config.js'],
+    options: {
+      extractors: [
+        {
+          extensions: ['mdx'],
+          extractor: (content) => {
+            content = mdx.sync(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
+
+            return broadMatches.concat(innerMatches);
+          },
+        },
+      ],
     },
   },
-  variants: {},
-  plugins: [],
 };
