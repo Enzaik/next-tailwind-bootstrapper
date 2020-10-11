17eb01a844221bf118a50ba5f31a20ce140d9c90
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-10-11 08:46:41 +0200

    feat: purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,17 @@ const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
     content: ['./components/**/*.js', './pages/**/*.js'],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    defaultExtractor:
+      // (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      // Capture as liberally as possible, including things like `h-(screen-1.5)`
+      (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+        // Capture classes within other delimiters like .block(className="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+        return broadMatches.concat(innerMatches);
+      },
   },
 ];
 module.exports = {
